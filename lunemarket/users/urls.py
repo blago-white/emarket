60a62b117a9dc3b8c4d105bbecb312db6f03d5fe
Bogdan Loginov <bogdanloginov31@gmail.com>
2023-05-30 16:25:36 +0300

    Added logout

--- lunemarket/users/urls.py
+++ lunemarket/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterUserView, LoginUserView
+from .views import RegisterUserView, LoginUserView, AccountView, LogoutUserView
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name="register"),
-    path('login/', LoginUserView.as_view(), name="login")
+    path('login/', LoginUserView.as_view(), name="login"),
+    path('account/<pk>', AccountView.as_view(), name="account"),
+    path('logout/', LogoutUserView.as_view(), name="logout")
 ]
--- lunemarket/users/views.py
+++ lunemarket/users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.forms import Form
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegisterUserForm
 from .filters import *
@@ -18,7 +18,16 @@ class RegisterUserView(CreateView):
 class LoginUserView(LoginView):
     form_class = AuthenticationForm
     template_name = "users/login.html"
-    success_url = reverse_lazy("account")
 
     def get_success_url(self):
         return reverse_lazy("home")
+
+
+class AccountView(DetailView):
+    model = User
+    template_name = "users/account.html"
+    context_object_name = "user"
+
+
+class LogoutUserView(LogoutView):
+    next_page = reverse_lazy("home")

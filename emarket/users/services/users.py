793cd4069379001588a36bd5eab5b11b208277c8
Bogdan Loginov <bogdanloginov31@gmail.com>
2023-09-30 20:49:05 +0300

    Small changes in UI

--- emarket/users/services/users.py
+++ emarket/users/services/users.py
@@ -7,12 +7,11 @@ from users.models.models import UserProfile
 def get_user_avatar_url(user_id: int) -> str | None:
     try:
         profile_avatar_name = UserProfile.objects.get(user__id=user_id).avatar.name
+        return str(settings.MEDIA_URL) + profile_avatar_name if profile_avatar_name else None
     except UserProfile.DoesNotExist:
         return SocialAccount.objects.get(user__id=user_id).get_avatar_url()
     except:
         return
-
-    return str(settings.MEDIA_URL) + profile_avatar_name if profile_avatar_name else None
 
 
 def get_username_by_mail(mail_adress: str) -> str:
